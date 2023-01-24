1f8404f74bac16b16a6cf9e970a2749487d7d070
diogenispanagiotis <diogenismusic@gmail.com>
2023-01-23 20:42:22 -0500

    remove block

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -41,14 +41,6 @@ const Nav = () => {
           <button onClick={handleCreatePost} type="button" className="hidden sm:flex text-black bg-white hover:bg-gray-100 font-medium rounded-full text-sm px-5 py-2 text-center mr-3 md:mr-0 border dark:bg-black dark:border-[#333] dark:text-white dark:hover:bg-[#111]">Create</button>
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          {/*<ul className="hover:bg-red flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-[#111] md:dark:bg-[#111] dark:border-gray-700">
-            <Link to={user ? `/p/${user.username}` : '/'}>
-              <button className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 dark:text-white" aria-current="page">Home</button>
-            </Link>
-            <Link to='/account'>
-              <button className="block py-2 pl-3 pr-4 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 md:dark:hover:text-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Account</button>
-            </Link>
-          </ul>*/}
         </div>
       </div>
     </nav>
